66e54b2f7a671f26f05a7fef7853a0d53cdc5c00
norhan-yasser <norhankhidr12@gmail.com>
2025-09-07 04:15:25 +0300

    task14 done

---
+++ TASK_14/task14_code/task14_code/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+button_driver\button.c
+
+DIO_driver\dio.c
+
+main.c
+
+Motor_driver\motor.c
+
